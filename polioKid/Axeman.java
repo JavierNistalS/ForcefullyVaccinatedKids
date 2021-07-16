988f4bdee806ad7a204f29436501acc79de98e3e
JavierNistalS <56133998+JavierNistalS@users.noreply.github.com>
2021-07-16 19:47:58 +0200

    Improved polioKid

--- polioKid/Axeman.java
+++ polioKid/Axeman.java
@@ -9,14 +9,19 @@ public class Axeman extends MyUnit {
         super(uc);
     }
 
+    int attackRound = 600;
+
     void playRound(){
         int[] info = uc.readSmokeSignals();
         if (info.length > 0){
             enemyBaseLocation = new Location(info[0]/100000, info[0]%100000);
         }
         generalAttack();
-        if (enemyBaseLocation != null){
+        if (enemyBaseLocation != null && (attackRound < uc.getRound() || uc.getLocation().distanceSquared(enemyBaseLocation) > 50)){
             move3(uc.getLocation().directionTo(enemyBaseLocation));
+        }
+        else if (enemyBaseLocation != null){
+            move5(enemyBaseLocation.directionTo(uc.getLocation()));
         }
         generalAttack();
     }
