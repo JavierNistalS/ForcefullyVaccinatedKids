33f22081ec411bb9b6353c8a508bd768f0e8599b
JavierNistalS <56133998+JavierNistalS@users.noreply.github.com>
2021-07-21 22:30:37 +0200

    Useful stuff

--- fullEconomy/Explorer.java
+++ fullEconomy/Explorer.java
@@ -1,15 +1,26 @@
 package fullEconomy;
 
-import aic2021.user.UnitController;
+import aic2021.user.*;
 
 public class Explorer extends MyUnit {
 
     Explorer(UnitController uc){
         super(uc);
+        pathfinding = new Pathfinding(uc);
     }
+
+    Exploration exploration;
+    Pathfinding pathfinding;
 
     void playRound(){
-
+        sustainTorch();
+        identifyBase();
+        if (exploration == null && baseLocation != null){
+            exploration = new Exploration(uc, baseLocation, 5);
+        }
+        ResourceInfo[] resources = uc.senseResources();
+        UnitInfo[] enemies = uc.senseUnits(uc.getTeam().getOpponent());
     }
+
 
 }
