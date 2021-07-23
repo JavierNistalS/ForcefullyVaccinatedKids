2a7522e21fb98d80d8f79780c7b7887dc67eaeb2
vbe5-ua <vbe5@gcloud.ua.es>
2021-07-23 20:19:34 +0200

    improved wolf micro

--- fullEconomy/Wolf.java
+++ fullEconomy/Wolf.java
@@ -33,50 +33,74 @@ public class Wolf extends MyUnit {
     boolean micro(){
         if (!uc.canMove())
             return false;
+
         UnitInfo[] enemies = uc.senseUnits(uc.getTeam().getOpponent());
         if (enemies.length == 0)
             return false;
-        int[] score = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for (Direction dir : dirs){
-            if (pathfinding.canMove(dir)){
-                Location loc = uc.getLocation().add(dir);
-                int light = uc.senseIllumination(loc);
-                if (light >= 8){
-                    score[dir.ordinal()] -= 100;
-                    for (UnitInfo ui : enemies){
-                        if (ui.getAttack() > 0 && ui.getLocation().distanceSquared(loc) <= ui.getType().attackRange + 10) {
-                            score[dir.ordinal()] -= ui.getAttack() * 100;
-                        }
-                    }
-                }
-                score[dir.ordinal()] -= light;
-                for (UnitInfo ui : enemies){
-                    score[dir.ordinal()] -= (int)(10*Math.sqrt(loc.distanceSquared(ui.getLocation())));
-                    if (ui.getType() == UnitType.BASE && loc.distanceSquared(ui.getLocation()) <= 40){
-                        score[dir.ordinal()] -= 10000;
-                        enemyBaseLocation = ui.getLocation();
-                    }
-                    else if (ui.getType() == UnitType.WOLF){
-                        int dist = loc.distanceSquared(ui.getLocation());
-                        if (dist <= 8)
-                            score[dir.ordinal()] -= 50;
-                        if (dist <= 2)
-                            score[dir.ordinal()] -= 50;
+
+        Direction bestDir = Direction.ZERO;
+        int bestScore = -10000000;
+
+        for (Direction dir : dirs) {
+            if (!pathfinding.canMove(dir))
+                continue;
+
+            int score = 0;
+            Location loc = uc.getLocation().add(dir);
+            int light = uc.senseIllumination(loc);
+
+            if (light >= 8) { // visible
+                score -= 50;
+                for (UnitInfo enemy : enemies) {
+                    if (enemy.getAttack() > 0 && enemy.getLocation().distanceSquared(loc) <= enemy.getType().attackRange + 10) {
+                        score -= enemy.getAttack() * 10;
                     }
                 }
             }
-        }
-        int bestScore = -100000000;
-        Direction best = Direction.ZERO;
-        for (Direction dir : dirs){
-            if (score[dir.ordinal()] > bestScore){
-                bestScore = score[dir.ordinal()];
-                best = dir;
+            score -= light / 2;
+            int canHitCount = 0;
+            boolean canHitSpearman = false;
+            int minDist = 0;
+
+            for (UnitInfo enemy : enemies) {
+
+                int dist = loc.distanceSquared(enemy.getLocation());
+                double linDist10 = (int)(10*Math.sqrt(dist));
+
+                if(uc.getType() == UnitType.AXEMAN)
+                    score += linDist10;
+                else if(uc.getType() == UnitType.SPEARMAN)
+                    score -= linDist10 * 2;
+                else
+                    score -= linDist10 / 2;
+
+                if(dist <= 2) {
+                    canHitCount++;
+                    canHitSpearman |= uc.getType() == UnitType.SPEARMAN;
+                }
+
+                if (enemy.getType() == UnitType.BASE) {
+                    enemyBaseLocation = enemy.getLocation();
+                }
+            }
+
+            if(canHitSpearman)
+                score += 1000;
+            if(canHitCount > 0)
+                score += 500 - canHitCount;
+
+            if(enemyBaseLocation != null && enemyBaseLocation.distanceSquared(loc) <= 18)
+                score -= 100000;
+
+            if(score > bestScore) {
+                bestScore = score;
+                bestDir = dir;
             }
         }
-        if (best != Direction.ZERO){
-            return pathfinding.tryMove(best);
-        }
+
+        if (bestDir != Direction.ZERO)
+            return pathfinding.tryMove(bestDir);
+
         return true;
     }
 
