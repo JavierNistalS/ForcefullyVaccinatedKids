9c6be3d1bd8424005a0cdc60d4f8c8392d4b511a
vbe5-ua <vbe5@gcloud.ua.es>
2021-07-25 23:34:28 +0200

    the forbidden class

--- chineseFluKid/TheKGB.java
+++ chineseFluKid/TheKGB.java
@@ -2,18 +2,53 @@ package chineseFluKid;
 
 import aic2021.user.*;
 
-public class TheKGB {
+
+public class TheKGB { // I only know that I know nothing, and so do you.
     UnitController uc;
     Communications comms;
 
     public TheKGB(UnitController uc){
         this.uc = uc;
         comms = new Communications(uc);
+
     }
 
     public boolean disruptRosa(Location loc){
-        int message = 1050*loc.x + loc.y + 180500000;
-        return trySmokeSignal(message);
+        //int message = 1050*loc.x + loc.y + 180500000;
+        //return trySmokeSignal(message);
+
+        if(loc == null) {
+            return trySmokeSignal(433461225 );
+        }
+        else {
+            Direction dir = uc.getLocation().directionTo(loc);
+
+            if(dir == Direction.NORTH || dir == Direction.NORTHEAST) // ++
+                return trySmokeSignal(431036316);
+            if(dir == Direction.EAST || dir == Direction.SOUTHEAST) // +-
+                return trySmokeSignal(431381955);
+            if(dir == Direction.SOUTH || dir == Direction.SOUTHWEST) // --
+                return trySmokeSignal(433461225);
+            //if(dir == Direction.WEST || dir == Direction.NORTHWEST) // -+
+                return trySmokeSignal(433894293);
+        }
+    }
+
+    public boolean disruptViper(Location loc) {
+        Location loc1 = new Location(974, 456);
+        Location loc2 = new Location(642, 762);
+        Location loc3 = new Location(656, 106);
+
+        int dist1 = loc1.distanceSquared(uc.getLocation());
+        int dist2 = loc2.distanceSquared(uc.getLocation());
+        int dist3 = loc3.distanceSquared(uc.getLocation());
+
+        if(dist1 > dist2 && dist1 > dist3)
+            return trySmokeSignal(7543);
+        else if(dist2 > dist3)
+            return trySmokeSignal(11243);
+        else
+            return trySmokeSignal(11223);
     }
 
     public boolean disruptCarbassots(Location loc){
@@ -37,12 +72,11 @@ public class TheKGB {
 
         uc.drawLineDebug(uc.getLocation(), loc, 120,0,0);
         double random = uc.getRandomDouble();
-        if (random < 0.333)
-            return disruptCarbassots(loc);
-        else if (random < 0.666)
+
+        if (random < 0.5)
             return disruptRosa(loc);
         else
-            return disruptWololo(loc, uc.getRandomDouble() < 0.5);
+            return disruptViper(loc);
     }
 
     public boolean trySmokeSignal(int x){
